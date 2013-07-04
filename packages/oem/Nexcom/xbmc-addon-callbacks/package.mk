51330822cdfb268fbebb5ce00da85bce52ed664d
Anton Serguienko <anton.sergienko@gmail.com>
2013-07-04 23:56:13 +0200

    Added Callbacks plugin.

--- packages/oem/Nexcom/xbmc-addon-callbacks/package.mk
+++ packages/oem/Nexcom/xbmc-addon-callbacks/package.mk
@@ -18,19 +18,40 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-PKG_NAME="Nexcom"
-PKG_VERSION=""
+PKG_NAME="xbmc-addon-callbacks"
+PKG_VERSION="master"
 PKG_REV="1"
 PKG_ARCH="any"
-PKG_LICENSE="various"
-PKG_SITE=""
-PKG_URL=""
-PKG_DEPENDS="xinput_calibrator xbmc-addon-callbacks"
-PKG_BUILD_DEPENDS="toolchain"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/pilluli/service.xbmc.callbacks"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+#PKG_URL="https://codeload.github.com/pilluli/service.xbmc.callbacks/tar.gz/master"
+PKG_DEPENDS_TARGET="toolchain Python"
 PKG_PRIORITY="optional"
-PKG_SECTION="oem"
-PKG_SHORTDESC="Nexcom"
-PKG_LONGDESC=""
+PKG_SECTION="mediacenter"
+PKG_SHORTDESC="Callbacks for XBMC."
+PKG_LONGDESC="Addon service for XBMC to call user defined scripts on specific callbacks such as player starts, stops, etc."
 PKG_IS_ADDON="no"
 
 PKG_AUTORECONF="no"
+
+make_target()
+{
+: #Empty
+}
+
+makeinstall_target()
+{
+	mkdir -p $INSTALL/usr/share/xbmc/addons/service.xbmc.callbacks
+ 	cp -PRf service.xbmc.callbacks-master/resources $INSTALL/usr/share/xbmc/addons/service.xbmc.callbacks
+ 	cp -Pf service.xbmc.callbacks-master/addon.xml $INSTALL/usr/share/xbmc/addons/service.xbmc.callbacks
+ 	cp -Pf service.xbmc.callbacks-master/default.py $INSTALL/usr/share/xbmc/addons/service.xbmc.callbacks
+}
+
+unpack()
+{
+	[ -d $PKG_BUILD ] && rm -rf $PKG_BUILD
+
+	mkdir -p $BUILD/${PKG_NAME}-${PKG_VERSION}
+	tar xf $SOURCES/${PKG_NAME}/${PKG_VERSION}.tar.gz -C $BUILD/${PKG_NAME}-${PKG_VERSION}
+}
